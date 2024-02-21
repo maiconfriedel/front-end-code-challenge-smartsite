0f678dfdefd3effc47aac53186aadbdaa9b89aab
Maicon Friedel <maicon.friedel@grupokyly.com.br>
2024-02-21 16:51:55 -0300

    minor adjustments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
-import Header from "@/components/header";
+import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <div className="bg-white">
       <Header />
-      <main className="py-10 px-20 flex">
-        <h1 className="whitespace-pre font-bold text-3xl">
-          REABERTURA SMART FIT
+      <main className="py-10 px-52 flex flex-col">
+        <h1 className="font-semibold text-3xl">
+          REABERTURA <br />
+          SMART FIT
         </h1>
+        <div className="bg-black h-[6px] w-16 mt-4" />
+        <p className="font-light mt-4 text-base">
+          O horário de funcionamento das nossas unidades está seguindo os
+          decretos de cada município. Por isso, confira aqui se a sua unidade
+          está aberta e as medida de segurança que estamos seguindo
+        </p>
       </main>
     </div>
   );
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,9 @@
 import Image from "next/image";
 
-const Header: React.FC = () => {
+export default function Header() {
   return (
     <header className="bg-black p-5 flex items-center justify-center">
       <Image src="logo.svg" width={150} height={0} alt="logo" />
     </header>
   );
-};
-
-export default Header;
+}
